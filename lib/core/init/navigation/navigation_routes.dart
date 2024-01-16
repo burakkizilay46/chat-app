29e40486f11e3aa732a6d29fb12e9abf927d9689
Burak Kızılay <burak.kizilay@westerops.com>
2024-01-16 23:38:47 +0300

    app arch created

---
+++ lib/core/init/navigation/navigation_routes.dart
@@ -0,0 +1,28 @@
+import 'package:chat_app/core/constants/navigation/navigation_constants.dart';
+import 'package:chat_app/core/init/navigation/navigation_exception.dart';
+import 'package:chat_app/features/home/view/home_view.dart';
+import 'package:chat_app/features/signin/view/signin_view.dart';
+import 'package:flutter/material.dart';
+
+class NavigationRoute {
+  static final NavigationRoute _instance = NavigationRoute._init();
+  static NavigationRoute get instance => _instance;
+
+  NavigationRoute._init();
+
+  Route<dynamic> generateRoute(RouteSettings args) {
+    switch (args.name) {
+      case NavigationConstants.DEFAULT:
+        return normalNavigate(const SignInView());
+      case NavigationConstants.HOME:
+        return normalNavigate(const HomeView());
+
+      default:
+        return normalNavigate(const HomeView());
+    }
+  }
+
+  MaterialPageRoute normalNavigate(Widget widget) {
+    return MaterialPageRoute(builder: (context) => widget);
+  }
+}
