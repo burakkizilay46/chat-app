3a182acf8c55a6f840e5664dc1c29f432e456ff8
Burak Kızılay <burak.kizilay@westerops.com>
2024-01-28 23:44:17 +0300

    firebase created rooms coll & fetch rooms data

--- lib/core/init/firebase/firebase_helper.dart
+++ lib/core/init/firebase/firebase_helper.dart
@@ -1,4 +1,5 @@
 import 'package:chat_app/providers/friends/model/friends_model.dart';
+import 'package:chat_app/providers/room/model/room_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FirebaseHelper {
@@ -13,4 +14,15 @@ class FirebaseHelper {
     // Burada allUsers değişkeni tüm kullanıcı verilerini içerir.
     return allUsers;
   }
+
+  Future<void> getRooms() async {
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+    CollectionReference rooms = firestore.collection('/rooms');
+
+    QuerySnapshot querySnapshot = await rooms.get();
+    final allRooms = querySnapshot.docs.map((doc) => doc.data()).toList();
+    print(allRooms);
+  }
 }
+
+/*  */
