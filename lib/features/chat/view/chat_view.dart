da2d41408240cbe413b7bfe5c37afe12807a2f0a
Burak Kızılay <burak.kizilay@westerops.com>
2024-02-03 23:46:54 +0300

    chat view refactor

--- lib/features/chat/view/chat_view.dart
+++ lib/features/chat/view/chat_view.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:chat_app/core/base/view/base_view.dart';
 import 'package:chat_app/core/components/chat_bubble/chat_bubble_view.dart';
 import 'package:chat_app/core/extensions/context_extansion.dart';
 import 'package:chat_app/providers/chat/chat_provider.dart';
@@ -8,90 +9,101 @@ import 'package:flutter/material.dart';
 import 'package:chat_app/providers/room/model/room_model.dart';
 import 'package:provider/provider.dart';
 
-class ChatView extends StatefulWidget {
+class ChatView extends StatelessWidget {
   final RoomsModel chats;
-
-  ChatView({
+  const ChatView({
     Key? key,
     required this.chats,
   }) : super(key: key);
 
   @override
-  State<ChatView> createState() => _ChatViewState();
-}
-
-class _ChatViewState extends State<ChatView> {
-  @override
   Widget build(BuildContext context) {
+    final ScrollController _scrollController = ScrollController();
     TextEditingController _controller = TextEditingController();
-    var currentUserId = context.read<UserProvider>().currentUser!.uid.toString();
-    String otherSenderId = '';
-    for (var message in widget.chats.messages) {
-      if (message.senderId != currentUserId) {
-        otherSenderId = message.senderId;
-        break;
+
+    void _scrollToBottom() {
+      if (_scrollController.hasClients) {
+        _scrollController.animateTo(
+          _scrollController.position.maxScrollExtent,
+          duration: Duration(milliseconds: 300),
+          curve: Curves.easeOut,
+        );
       }
     }
 
-    return Scaffold(
-        appBar: AppBar(
-          actions: [
-            IconButton(
-                onPressed: () => context.read<ChatProvider>().listenToMessages(widget.chats.id),
-                icon: Icon(Icons.refresh))
-          ],
-          title: Text(widget.chats.id, style: context.normalTextStyle.copyWith(fontSize: 12)),
-        ),
-        body: Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Flexible(
-                flex: 88,
-                child: Consumer<ChatProvider>(
-                  builder: (context, chatProvider, child) {
-                    return ListView.builder(
-                        itemCount: chatProvider.messages.length,
-                        itemBuilder: (context, index) {
-                          var message = chatProvider.messages[index];
-                          var isSender = context.read<UserProvider>().currentUser!.uid.toString() == message.senderId;
-
-                          return ChatBubble(
-                            message: message.content,
-                            isSender: isSender,
-                          );
-                        });
-                  },
-                ),
-              ),
-              Flexible(
-                flex: 10,
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: SizedBox(
-                        height: 48,
-                        child: TextField(
-                          controller: _controller,
-                          decoration: InputDecoration(
-                              label: Text('Mesaj'),
-                              border: OutlineInputBorder(borderRadius: BorderRadius.circular(100))),
-                        ),
-                      ),
+    return BaseView(
+      provider: ChatProvider(),
+      onProviderReady: (ChatProvider provider) {
+        provider.listenToMessages(chats.id);
+      },
+      onPageBuilder: (ChatProvider provider) {
+        return Scaffold(
+            appBar: AppBar(
+              actions: [
+                IconButton(
+                    onPressed: () => context.read<ChatProvider>().listenToMessages(chats.id), icon: Icon(Icons.refresh))
+              ],
+              title: Text(chats.id, style: context.normalTextStyle.copyWith(fontSize: 12)),
+            ),
+            body: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Flexible(
+                    flex: 88,
+                    child: Consumer<ChatProvider>(
+                      builder: (context, chatProvider, child) {
+                        if (chatProvider.messages.isEmpty) {
+                          // Liste boşsa gösterilecek widget
+                          return Center(child: Text("Mesaj yok"));
+                        }
+                        return ListView.builder(
+                          controller: _scrollController,
+                          itemCount: chatProvider.messages.length,
+                          itemBuilder: (context, index) {
+                            var message = chatProvider.messages[index];
+                            var isSender = context.read<UserProvider>().currentUser!.uid.toString() == message.senderId;
+                            return ChatBubble(
+                              message: message.content,
+                              isSender: isSender,
+                            );
+                          },
+                        );
+                      },
                     ),
-                    IconButton(
-                        onPressed: () {
-                          context.read<ChatProvider>().sendMessage(
-                              widget.chats.id, _controller.text.trim(), context.read<UserProvider>().currentUser!.uid);
-                          _controller.clear();
-                          setState(() {});
-                        },
-                        icon: Icon(Icons.send))
-                  ],
-                ),
-              )
-            ],
-          ),
-        ));
+                  ),
+                  Flexible(
+                    flex: 10,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: SizedBox(
+                            height: 48,
+                            child: TextField(
+                              controller: _controller,
+                              decoration: InputDecoration(
+                                  label: Text('Mesaj'),
+                                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(100))),
+                            ),
+                          ),
+                        ),
+                        IconButton(
+                            onPressed: () {
+                              context
+                                  .read<ChatProvider>()
+                                  .sendMessage(
+                                      chats.id, _controller.text.trim(), context.read<UserProvider>().currentUser!.uid)
+                                  .then((value) => _scrollToBottom);
+                              _controller.clear();
+                            },
+                            icon: Icon(Icons.send))
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ));
+      },
+    );
   }
 }
